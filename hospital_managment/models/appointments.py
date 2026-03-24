1413a326aa9b092a26f8d97333a1ca2fa0c5d9a3
ahmedalaa12484 <az.ahmed.alaa@gmail.com>
2026-03-24 11:11:07 +0200

    [add] appointments

--- hospital_managment/models/appointments.py
+++ hospital_managment/models/appointments.py
@@ -7,3 +7,5 @@ class Appointments(models.Model):
 
     name=fields.Char(string="Name",tracking=1)
     patient_id=fields.Many2one('hospital.patient')
+    appointment_time=fields.Datetime(string="Appointment Time",default=fields.Datetime.now,tracking=1)
+    booking_date=fields.Date(string="Booking Date",tracking=1,default=fields.Date.context_today)
