ad4ab1d49bcef24fb91152ac6cf7a11b765669d9
ahmedalaa12484 <az.ahmed.alaa@gmail.com>
2026-03-25 03:03:53 +0200

    compute field

--- hospital_managment/models/appointments.py
+++ hospital_managment/models/appointments.py
@@ -7,5 +7,6 @@ class Appointments(models.Model):
 
     name=fields.Char(string="Name",tracking=1)
     patient_id=fields.Many2one('hospital.patient')
+    gender = fields.Selection(related='patient_id.gender',string="Gender",readonly=False)
     appointment_time=fields.Datetime(string="Appointment Time",default=fields.Datetime.now,tracking=1)
     booking_date=fields.Date(string="Booking Date",tracking=1,default=fields.Date.context_today)
--- hospital_managment/models/patient.py
+++ hospital_managment/models/patient.py
@@ -8,10 +8,16 @@ class Patient(models.Model):
     _inherit=['mail.thread','mail.activity.mixin']
 
     name = fields.Char(string="Patient Name")
-    age = fields.Integer(string="age")
+    age = fields.Integer(string="age",compute='_compute_calc_age',store=True)
     ref = fields.Char(string="Reference")
     active = fields.Boolean(string="active")
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'FeMale'),
     ],string="gender",required=True,default='male')
+    date_of_birth = fields.Date(string="Date of Birth")
+
+
+    def _compute_calc_age(self):
+        for rec in self:
+            pass
