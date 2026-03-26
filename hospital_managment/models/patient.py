653d2a98d5d0778193ca246b977fbd0442c0bbaa
ahmedalaa404@github.com <az.ahned.alaaa@gmail.com>
2026-03-26 17:19:37 +0200

    [notbook-compute(field)-rec_name_onchange]

--- hospital_managment/models/patient.py
+++ hospital_managment/models/patient.py
@@ -1,23 +1,32 @@
+import datetime
+
 from odoo import api, fields, models, _
 
 
 class Patient(models.Model):
-
     _name = 'hospital.patient'
     _description = 'Patient'
-    _inherit=['mail.thread','mail.activity.mixin']
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char(string="Patient Name")
-    age = fields.Integer(string="age",compute='_compute_calc_age',store=True)
+    age = fields.Integer(string="age", compute='_compute_calc_age', store=True)
     ref = fields.Char(string="Reference")
     active = fields.Boolean(string="active")
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'FeMale'),
-    ],string="gender",required=True,default='male')
+    ], string="gender", required=True, default='male')
     date_of_birth = fields.Date(string="Date of Birth")
 
-
+    @api.depends('date_of_birth')
     def _compute_calc_age(self):
         for rec in self:
-            pass
+            if rec.date_of_birth:
+                year_of_now=datetime.datetime.now().year
+                year_of_birth=rec.date_of_birth.year
+                print(year_of_birth)
+                print(year_of_now)
+                rec.age = year_of_now - year_of_birth
+            else:
+                rec.age = 0
+
