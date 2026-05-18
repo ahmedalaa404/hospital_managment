d5edaee92b5203cee485b8247cb043b30bda8ca9
ahmedalaa12484 <az.ahmed.alaa@gmail.com>
2026-05-19 01:09:56 +0300

    [override ] orm methode copy it work befor constrains and create u can use it when make duplicate

--- hospital_managment/models/hospital_patient_tag.py
+++ hospital_managment/models/hospital_patient_tag.py
@@ -1,16 +1,23 @@
-from odoo import api,fields,models
+from odoo import api, fields, models
+
 
 class HospitalPatientTag(models.Model):
     _name = 'hospital.patient.tag'
 
-    name=fields.Char(string="Name")
-    color=fields.Integer(string="color picker")
-    color_Picker2=fields.Char(string="color_Picker2")
-    active=fields.Boolean(default=True)
-    sequence=fields.Integer(default=0)
+    name = fields.Char(string="Name")
+    color = fields.Integer(string="color picker")
+    color_Picker2 = fields.Char(string="color_Picker2")
+    active = fields.Boolean(default=True,copy=False)
+    sequence = fields.Integer(default=0)
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        default.setdefault('name', f'{self.name}-(copy)')
+
+        return super().copy(default)
 
     _sql_constraints = [
-        ('unique_name','unique(name,active)','Name must be unique')
+        ('unique_name', 'unique(name,active)', 'Name must be unique')
     ]
 
 
