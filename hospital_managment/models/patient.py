82027a35f4608eea9446919717ab91fda1f1bc1b
ahmedalaa404 <az.ahmed.alaa@gmail.com>
2026-06-01 18:40:03 +0300

    [add] radio for selection and make inverse function and make search function for thing not store

--- hospital_managment/models/patient.py
+++ hospital_managment/models/patient.py
@@ -1,6 +1,10 @@
-import datetime
-from odoo import api, fields, models,_
+from calendar import month
+
+from odoo import api, fields, models, _
+
 from odoo.exceptions import ValidationError
+from datetime import datetime, timedelta, date
+from dateutil.relativedelta import relativedelta
 
 
 class Patient(models.Model):
@@ -8,26 +12,26 @@ class Patient(models.Model):
     _description = 'Patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(string="Patient Name",required=True)
-    age = fields.Integer(string="age", compute='_compute_calc_age', store=True)
-    ref = fields.Char(string="Reference",help="This refers to the patient is identity")
-    active = fields.Boolean(string="active",default=True)
+    name = fields.Char(string="Patient Name", required=True)
+    date_of_birth = fields.Date(string="Date of Birth")
+    age = fields.Integer(string="age", compute='_compute_calc_age', inverse='_inverse_compute_age',search='_search_age')
+    ref = fields.Char(string="Reference", help="This refers to the patient is identity")
+    active = fields.Boolean(string="active", default=True)
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'FeMale'),
     ], string="gender", required=True, default='male')
-    date_of_birth = fields.Date(string="Date of Birth")
-    color=fields.Integer(string="Color",required=True)
-    color2=fields.Char(string="Color2")
 
-    tag_ids=fields.Many2many('hospital.patient.tag')
+    color = fields.Integer(string="Color", required=True)
+    color2 = fields.Char(string="Color2")
 
-    appointments_ids=fields.One2many('hospital.appointments', 'patient_id', string="Appointments")
-    appointments_count=fields.Integer(string="Appointments Count",compute="_compute_appointments_count",store=True)
+    tag_ids = fields.Many2many('hospital.patient.tag')
 
+    appointments_ids = fields.One2many('hospital.appointments', 'patient_id', string="Appointments")
+    appointments_count = fields.Integer(string="Appointments Count", compute="_compute_appointments_count", store=True)
 
-    parent =fields.Char(string="Parent Patient")
-    partner_name =fields.Char(string="partner name")
+    parent = fields.Char(string="Parent Patient")
+    partner_name = fields.Char(string="partner name")
     marital_status = fields.Selection([
         ('single', 'Single'),
         ('married', 'Married'),
@@ -35,64 +39,57 @@ class Patient(models.Model):
         ('widowed', 'Widowed'),
     ], string="Marital Status")
 
-
-
-
     @api.depends('appointments_ids')
     def _compute_appointments_count(self):
         for rec in self:
-            rec.appointments_count=self.env['hospital.appointments'].search_count(
-                [('patient_id','=',rec.id)])
+            rec.appointments_count = self.env['hospital.appointments'].search_count(
+                [('patient_id', '=', rec.id)])
+
     @api.model
     def create(self, vals_list):
-        vals_list['ref']=self.env['ir.sequence'].next_by_code('sequence.patient')
-        return super(Patient,self).create(vals_list)
+        vals_list['ref'] = self.env['ir.sequence'].next_by_code('sequence.patient')
+        return super(Patient, self).create(vals_list)
 
-    def write(self,vals):
-        if not self.ref or not vals.get('ref') :
-           vals['ref']=self.env['ir.sequence'].next_by_code('sequence.patient')
+    def write(self, vals):
+        if not self.ref or not vals.get('ref'):
+            vals['ref'] = self.env['ir.sequence'].next_by_code('sequence.patient')
         return super().write(vals)
+
     @api.depends('date_of_birth')
     def _compute_calc_age(self):
         for rec in self:
             if rec.date_of_birth:
-                year_of_now=datetime.datetime.now().year
-                year_of_birth=rec.date_of_birth.year
+                year_of_now = datetime.now().year
+                year_of_birth = rec.date_of_birth.year
                 print(year_of_birth)
                 print(year_of_now)
                 rec.age = year_of_now - year_of_birth
             else:
                 rec.age = 0
 
-
     def name_get(self):
-        patients_name=[]
+        patients_name = []
         for rec in self:
-            name=rec.name+" "+rec.ref
-            patients_name.append((rec.id,name))
+            name = rec.name + " " + rec.ref
+            patients_name.append((rec.id, name))
 
         return patients_name
 
-
-
     def default_get(self, fields_list):
         print(fields_list)
-        return super(Patient,self).default_get(fields_list)
-
+        return super(Patient, self).default_get(fields_list)
 
     @api.constrains('date_of_birth')
     def _check_date_of_birth(self):
         for rec in self:
-            if rec.date_of_birth and rec.date_of_birth>fields.Date.today() and rec.date_of_birth>fields.Date.today():
+            if rec.date_of_birth and rec.date_of_birth > fields.Date.today() and rec.date_of_birth > fields.Date.today():
                 raise ValidationError(_("The Enter data is Not acceptable"))
-
 
     @api.model
     def default_get(self, fields_list):
         print(self)
         print(fields_list)
         return super().default_get(fields_list)
-
 
     @api.ondelete(at_uninstall=False)
     def check_appointments(self):
@@ -101,5 +98,26 @@ class Patient(models.Model):
                 raise ValidationError(_("U Can`t delete patient if have appointments"))
 
     def action_appear_group_by(self):
-            for rec in self:
-              print(rec)
+        for rec in self:
+            print(rec)
+
+    @api.onchange('age')
+    @api.depends('age')
+    def _inverse_compute_age(self):
+        for rec in self:
+            if rec.age:
+                rec.date_of_birth = date.today() - relativedelta(years=rec.age)
+
+
+    def _search_age (self, operator,value):
+        print("-------------------------------------------------------")
+        print(operator)
+        print(value)
+        start_date=(date.today() - relativedelta(years=value)).replace(day=1,month=1)
+        end_date=(date.today() - relativedelta(years=value)).replace(day=31,month=12)
+        print("----start_date-----",start_date)
+        print("----end_date-----",end_date)
+        print("-------------------------------------------------------")
+
+        # start_date=date.date(month=1,days=1)
+        return [('date_of_birth','>=',start_date),('date_of_birth','<=',end_date)]
--- hospital_managment/models/res_config_settings.py
+++ hospital_managment/models/res_config_settings.py
@@ -3,4 +3,5 @@ from odoo import models,fields
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    cancel_days=fields.Integer(string="Cancel Days")
+    cancel_days=fields.Integer(string="Cancel Days",
+                               config_parameter="hospital_managment.cancel_days")
