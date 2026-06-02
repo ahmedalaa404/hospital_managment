90b7b1fd2a03bf98e9d75f878fe14a4981133fa4
ahmedalaa12484 <az.ahmed.alaa@gmail.com>
2026-06-03 01:34:03 +0300

    make field refernce in odoo

--- hospital_managment/models/operation.py
+++ hospital_managment/models/operation.py
@@ -1,16 +1,19 @@
-from odoo import models,fields,api
+from odoo import models, fields, api
 
 
 class Operation(models.Model):
     _name = 'operation.operation'
     _description = 'Operation'
-    _log_access=False
+    _log_access = False
     _rec_name = 'ref'
 
-    doctor_id=fields.Many2one('res.users',string='Doctor')
-    ref=fields.Char()
-    operation_name=fields.Char()
-
+    doctor_id = fields.Many2one('res.users', string='Doctor')
+    ref = fields.Char()
+    operation_name = fields.Char()
+    ref_record = fields.Reference(
+        [('hospital.patient', 'patient'),
+         ('hospital.appointments', 'appointments')],
+        string='record')
 
     @api.model
     def name_create(self, name):
